c058820d7d7046926aa395c2c7295594e1293768
cortadocodes <cortado.codes@protonmail.com>
2023-02-23 12:00:36 +0000

    ENH: Add latest octue versions

--- cli.py
+++ cli.py
@@ -7,6 +7,9 @@ from inter_service_compatibility.record_questions_across_versions import record_
 
 
 VERSIONS_TO_CHECK = [
+    "0.43.6",
+    "0.43.5",
+    "0.43.4",
     "0.43.3",
     "0.43.2",
     "0.43.1",
